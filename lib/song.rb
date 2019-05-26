e5c2ac15f186ddb3bbd5caa3b16bbdaff303c79c
Trish Ogen <patriciaogen@gmail.com>
2019-05-26 17:37:30 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -16,9 +16,9 @@ class Song
      song
   end
 
-  def self.new_by_name
-    song = Song.new(name)
+  def self.new_by_name(name)
+    song = Song.new
+    song.name = name
     @@all <<song
-    song
   end
 end
